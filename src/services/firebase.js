068b360043ff11c75e88ae09fe242db5a6e453cd
Rohan Kumar <rohan@heberway.com>
2021-05-15 20:53:12 -0700

    fix

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-export const round = 1;
+export const round = 2;
 
 const firebaseConfig = {
     apiKey: "AIzaSyA1ibbAUUiCS1n9vE0PChw0k5tCRwrgJI0",
