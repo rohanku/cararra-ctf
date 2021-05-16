56ac27123354bc34a4b446e83dfa4651189f533b
Rohan Kumar <rohan@heberway.com>
2021-05-15 17:55:17 -0700

    maintenance

--- src/App.js
+++ src/App.js
@@ -4,47 +4,13 @@ import "fontsource-roboto";
 import { ThemeProvider } from "@material-ui/core/styles";
 
 import theme from "./theme";
-import SignIn from "./pages/SignIn";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import UserProvider from "./providers/UserProvider";
-import GlobalProvider from "./providers/GlobalProvider";
-import LeaderboardProvider from "./providers/LeaderboardProvider";
-import { getBot } from "./services/firebase";
 
 function App() {
-  const routes = [
-    { path: ["/dashboard", "/challenges", "/leaderboard"], Component: Home },
-    { path: "/signin", Component: SignIn },
-    { path: "/register", Component: Register },
-  ];
-  getBot();
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
-          <Router>
-            <GlobalProvider>
-              <UserProvider>
-                <LeaderboardProvider>
-                  <Switch>
-                    {routes.map(({ path, Component, props }) => (
-                      <Route key={path} exact path={path}>
-                        <Component {...props} />
-                      </Route>
-                    ))}
-                    <Redirect to="/signin" />
-                  </Switch>
-                </LeaderboardProvider>
-              </UserProvider>
-            </GlobalProvider>
-          </Router>
+          Sorry! The website is down for maintenance. Message cararra or rohankk for links if necessary.
         </header>
       </div>
     </ThemeProvider>
